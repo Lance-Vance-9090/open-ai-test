197d02d9715fa46534de4136b6b50c358218c2d9
mhassan581 <132934539+mhassan581@users.noreply.github.com>
2023-09-29 17:17:15 +0500

    updated

--- index.js
+++ index.js
@@ -7,46 +7,44 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-// const message = "My name is David";
-// const context = "Making you learn about my personal information";
+let messageHistory = [];
+
+let firstCall = [
+  {
+    role: "system",
+    content:
+      "You are an assistant who is asking some questions related to user's personal information",
+  },
+  { role: "assistant", content: "What is your name?" },
+  { role: "user", content: "My name is David" },
+  { role: "assistant", content: "What is your age?" },
+  { role: "user", content: "it is 22 years old" },
+  {
+    role: "user",
+    content:
+      "Is my answer above related to the age question you asked? Reply in yes or no",
+  },
+];
 
 const response = await openai.chat.completions.create({
   model: "gpt-3.5-turbo",
-  messages: [
-    {
-      role: "system",
-      content:
-        "You are an assistant who is asking some questions related to user's personal information",
-    },
-    { role: "assistant", content: "What is your name?" },
-    { role: "user", content: "My name is David" },
-    {
-      role: "user",
-      content:
-        "Is my answer above related to the age question you asked? Reply in yes or no",
-    },
-  ],
+  messages: firstCall,
   temperature: 0,
   max_tokens: 150,
 });
 
-// const response1 = await openai.chat.completions.create({
-//   model: "gpt-3.5-turbo",
-//   messages: [
-//     {
-//       role: "system",
-//       content: `You are an assistant who is asking some questions related to user's personal information. Refer to the previous chat : ${response.id}`,
-//     },
-//     { role: "assistant", content: "What is your age?" },
-//     { role: "user", content: "22 years old" },
-//     {
-//       role: "user",
-//       content: "Please tell my name again",
-//     },
-//   ],
-//   temperature: 0,
-//   max_tokens: 150,
-// });
+messageHistory.push(firstCall);
+
+const obj = { role: "user", content: "What is my age and name?" };
+
+messageHistory[0].push(obj);
+
+const response1 = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: messageHistory[0],
+  temperature: 0,
+  max_tokens: 150,
+});
 
 console.log(response.choices);
-// console.log(response1.choices);
+console.log(response1.choices);
